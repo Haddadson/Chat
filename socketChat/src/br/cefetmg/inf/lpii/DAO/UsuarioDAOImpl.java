28db8e7980bd1d00070c61a77319509150909fa6
brenomariz <bacmariz@yahoo.com.br>
2017-10-01 21:36:07 -0300

    UsuarioDAO Implementation

--- socketChat/src/br/cefetmg/inf/lpii/DAO/UsuarioDAOImpl.java
+++ socketChat/src/br/cefetmg/inf/lpii/DAO/UsuarioDAOImpl.java
@@ -1,37 +1,145 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package br.cefetmg.inf.lpii.DAO;
 
 import br.cefetmg.inf.lpii.DAO.interfaces.UsuarioDAO;
 import br.cefetmg.inf.lpii.entities.Usuario;
+import br.cefetmg.inf.lpii.exception.PersistenceException;
+import br.cefetmg.inf.lpii.util.db.ConnectionManager;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
- * @author Aluno
+ * @author Breno Mariz
  */
 public class UsuarioDAOImpl implements UsuarioDAO {
 
+    private static UsuarioDAOImpl userDAO = null;
+    
+    private UsuarioDAOImpl() {}
+    
+    private static UsuarioDAOImpl getInstance() {
+        if (userDAO == null) {
+            userDAO = new UsuarioDAOImpl();
+        }
+        return userDAO;
+    }
+    
     @Override
-    public Long inserir(Usuario usuario) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Long inserir(Usuario usuario) throws PersistenceException {
+        if (usuario == null) {
+            throw new PersistenceException("Domain cannot be null");
+        }
+
+        Long userId = null;
+
+        try {
+            Connection connection = ConnectionManager.getInstance().getConnection();
+
+            String sql = "INSERT INTO \"Usuario\" (COD_usuario, NOM_usuario) "
+                    + "    VALUES (?, ?) returning COD_usuario;";
+
+            PreparedStatement pstmt = connection.prepareStatement(sql);
+            pstmt.setLong(1, usuario.getId());
+            pstmt.setString(2, usuario.getNome());
+            ResultSet rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                userId = rs.getLong("COD_usuario");
+                usuario.setId(userId);
+            }
+
+            rs.close();
+            pstmt.close();
+            connection.close();
+
+        } catch (ClassNotFoundException | SQLException ex) {
+            ex.getMessage();
+        }
+
+        return userId;
     }
 
     @Override
-    public boolean atualizar(Usuario usuario) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public boolean atualizar(Usuario usuario) throws PersistenceException {
+        try {
+
+            Connection connection = ConnectionManager.getInstance().getConnection();
+
+            String sql = "UPDATE \"Usuario\" "
+                    + " SET NOM_usuario = ? "
+                    + " WHERE COD_usuario = ?";
+
+            PreparedStatement pstmt = connection.prepareStatement(sql);
+            pstmt.setString(1, usuario.getNome());
+            pstmt.setLong(5, usuario.getId());
+            pstmt.executeUpdate();
+
+            pstmt.close();
+            connection.close();
+
+            return true;
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+                throw new PersistenceException(e);
+        }
+
     }
 
     @Override
-    public boolean remover(Long id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public boolean remover(Long id) throws PersistenceException {
+        try {
+            Connection connection = ConnectionManager.getInstance().getConnection();
+
+            String sql = "DELETE FROM \"usuario\" WHERE COD_usuario = ?";
+
+            PreparedStatement pstmt = connection.prepareStatement(sql);
+            pstmt.setLong(1, id);
+            pstmt.executeUpdate();
+
+            pstmt.close();
+            connection.close();
+
+            return true;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new PersistenceException(e);
+        }
     }
 
     @Override
-    public Usuario get(Long id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Usuario get(Long id) throws PersistenceException {
+        try {
+            Connection connection = ConnectionManager.getInstance().getConnection();
+
+            String sql = "SELECT * FROM \"Usuario\" WHERE COD_usuario = ? ";
+
+            PreparedStatement pstmt = connection.prepareStatement(sql);
+            pstmt.setLong(1, id);
+            ResultSet rs = pstmt.executeQuery();
+
+            Usuario usuario = null;
+            if (rs.next()) {
+                usuario = new Usuario();
+                usuario.setId(id);
+                usuario.setNome(rs.getString("NOM_usuario"));               
+            }
+
+            rs.close();
+            pstmt.close();
+            connection.close();
+
+            return usuario;
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new PersistenceException(e);
+        }
     }
     
 }
