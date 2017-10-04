03ffe2a0a8d11724b4480a516641404d671b238b
Haddadson <gabriel.haddad15@gmail.com>
2017-10-04 19:39:59 -0300

    Create JavaFX project

---
+++ socketChatView/src/socketchatview/FXMLDocumentController.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package socketchatview;
+
+/*
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+*/
+
+/**
+ *
+ * @author aluno
+ */
+public class FXMLDocumentController {
+     //implements Initializable 
+         
+}
