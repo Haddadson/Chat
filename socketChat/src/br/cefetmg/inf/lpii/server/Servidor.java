7fd9a34e80d289194101c0c2f5cd19584d56a214
Haddadson <gabriel.haddad15@gmail.com>
2017-10-05 09:40:21 -0300

    Renames folder

--- socketChat/src/br/cefetmg/inf/lpii/server/Servidor.java
+++ socketChat/src/br/cefetmg/inf/lpii/server/Servidor.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.cefeftmg.inf.lpii.server;
+package br.cefetmg.inf.lpii.server;
 
 import java.io.IOException;
 import java.net.ServerSocket;
