2efa9438c21d56ac155315f5d2d29cefd673cf5b
Haddadson <gabriel.haddad15@gmail.com>
2017-10-10 18:51:06 -0300

    Removes imports at UsuarioDAOImpl

--- socketChat/src/br/cefetmg/inf/lpii/DAO/UsuarioDAOImpl.java
+++ socketChat/src/br/cefetmg/inf/lpii/DAO/UsuarioDAOImpl.java
@@ -9,9 +9,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
+
 
 /**
  *
